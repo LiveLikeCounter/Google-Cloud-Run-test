5f755fe7575e9da12a46514e82b2bb8f30e7f23b
Johan Walhout <johanwalhout@gmail.com>
2019-12-10 12:48:47 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import * as firebase from "firebase/app";
+import 'firebase/analytics';
+firebase.initializeApp(environment.firebaseConfig)
+firebase.analytics();
+
+import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyChNhNIHldl-bbpQxzQyu5NhGyw0wSSRSM",
+    authDomain: "interestinate-website-ongcloud.firebaseapp.com",
+    databaseURL: "https://interestinate-website-ongcloud.firebaseio.com",
+    projectId: "interestinate-website-ongcloud",
+    storageBucket: "interestinate-website-ongcloud.appspot.com",
+    messagingSenderId: "724887639796",
+    appId: "1:724887639796:web:8f406e19073123fd6570a1",
+    measurementId: "G-13QWWQ30CQ"
+  }
 };
 
 /*
